fd0d5f4777027e54e5b4b192eec5c8904aa05fb5
Dmitry Rakov <rakovdk@gmail.com>
2023-02-17 23:42:39 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        //I just want to add some comments in order to commit and push
         System.out.println("Hello world!");
     }
 }
